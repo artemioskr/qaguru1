e39cb3a576a8ea58291a0c70f866c857a5c6a76a
Artemiy Skryabin <artemio.skr@gmail.com>
2021-01-31 16:00:44 +0300

    add gradle and PracticeFormTest

---
+++ src/tests/PracticeFormTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
+public class PracticeFormTest {
+
+    @Test
+    public void fillPracticeForm() {
+        open("https://ya.ru");
+    }
+}
