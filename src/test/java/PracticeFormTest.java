835c59797a842ea654a988fbce8c5016cba8614e
Artemiy Skryabin <artemio.skr@gmail.com>
2021-02-01 18:39:07 +0300

    add asserts

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,7 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import java.io.File;
 
@@ -20,32 +19,51 @@ public class PracticeFormTest {
     }
 
     @Test
-    public void fillFieldsPracticeForm() throws InterruptedException {
+    public void fillFieldsPracticeForm() {
+
+        String firstName = "Alexandr";
+        String lastName  = "Ivanov";
+        String email     = "useremail@here.ru";
+        String gender    = "Male";
+        String phone     = "9851234567";
+        String subject   = "English";
+        String hobbie    = "Sports";
+        String address   = "moscow,1,3,2";
+        String state     = "NCR";
+        String city      = "Delhi";
 
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Alexandr");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("useremail@here.ru");
-        $(byText("Male")).click();
-        $("#userNumber").setValue("9851234567");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(phone);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").$(byValue("1994")).click();
         $(".react-datepicker__month-select").$(byValue("0")).click();
         $(".react-datepicker__month").$(byText("5")).click();
-        $("#subjectsInput").setValue("English").pressEnter();
-        $(byText("Sports")).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $(byText(hobbie)).click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/frog.jpg"));
-        $("#currentAddress").setValue("moscow,1,3,2");
+        $("#currentAddress").setValue(address);
         $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#stateCity-wrapper").$(byText(city)).click();
         $("#submit").click();
 
-
-
-        Thread.sleep(3000);
-
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName + " " + lastName));
+        $(".table-responsive").shouldHave(text(email));
+        $(".table-responsive").shouldHave(text(gender));
+        $(".table-responsive").shouldHave(text(phone));
+        $(".table-responsive").shouldHave(text("05 January,1994"));
+        $(".table-responsive").shouldHave(text(subject));
+        $(".table-responsive").shouldHave(text(hobbie));
+        $(".table-responsive").shouldHave(text("frog.jpg"));
+        $(".table-responsive").shouldHave(text(address));
+        $(".table-responsive").shouldHave(text(state + " " + city));
     }
 }
