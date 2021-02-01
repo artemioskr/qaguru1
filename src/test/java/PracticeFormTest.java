51aebb71f44f5b557be3a262b5417518a2e94b0a
Artemiy Skryabin <artemio.skr@gmail.com>
2021-02-01 17:55:42 +0300

    upload files

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,6 +1,11 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byValue;
 import static com.codeborne.selenide.Selenide.*;
@@ -8,8 +13,14 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
 
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+
+    }
+
     @Test
-    public void fillFieldsPracticeForm() {
+    public void fillFieldsPracticeForm() throws InterruptedException {
 
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue("Alexandr");
@@ -23,11 +34,18 @@ public class PracticeFormTest {
         $(".react-datepicker__month").$(byText("5")).click();
         $("#subjectsInput").setValue("English").pressEnter();
         $(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/frog.jpg"));
         $("#currentAddress").setValue("moscow,1,3,2");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
 
+
+
+        Thread.sleep(3000);
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
     }
 }
